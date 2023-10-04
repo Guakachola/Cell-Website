b5bbb3f50fe868b29d1e12846009ff6355f8c0fc
Guakachola <ClarkeJohnsonTX@gmail.com>
2023-10-03 22:00:52 -0500

    Update code

--- myScript.js
+++ myScript.js
@@ -1,5 +1,5 @@
 function showOrganelle(){
-    var selectedOrganelle = document.getElementById("organelle").value;
+    var selectedOrganelle = document.getElementById("organelleSelect").value;
 
     var organelleModels = {
         "Nucleolus": "assets/Nucleolus.glb",
